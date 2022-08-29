a93645906c5542bb2a022bf91ade575228d140ec
Rabia Çakmak <ozancakmak867@gmail.com>
2022-08-29 14:07:21 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_ElseIf_Ternary_If
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+# If-ElseIf-Ternary-If
+
+            //If-ElseIf-Ternary-If
+            int time = DateTime.Now.Hour;
+            if (time >= 6 && time < 11)
+            {
+                Console.WriteLine("Günaydın");
+            }
+
+            else if (time <= 18)
+            {
+                Console.WriteLine("iyi günler");
+            }
+            else
+            {
+                Console.WriteLine("İyi akşamlar");
+            }
+
+        }
+    }
+}
+    }
+}
